44179c2bb6a9599ebd7b8a34f9d275ba38808e85
Mahee Hossain <mahee.hossain@student.unimelb.edu.au>
2021-08-31 23:07:07 +1000

    set up home page

--- backend/models/Customer.js
+++
@@ -1,9 +0,0 @@
-const Datastore = require('nedb');
-
-const CustomerDb = new Datastore({
-    filename:'./databases/customers.db',
-    autoload: true,
-    corruptAlertThreshold: 1
-});
-
-module.exports = CustomerDb;
